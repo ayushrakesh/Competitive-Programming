cc3f464942829160c428d6f57cb048b64bd27ce1
Ayush Rakesh <ayushrakesh35711artlover@gmail.com>
2024-01-28 23:31:13 +0530

    PLus minus split

--- plus_minus_split.cpp
+++ plus_minus_split.cpp
@@ -7,57 +7,19 @@ int main()
 
   while (cases != 0)
   {
-    int n = 0;
+    long long n, pos = 0;
     cin >> n;
-
     string s;
     cin >> s;
 
-    vector<int> v;
-
-    for (int i = 0; i < s.length(); i++)
+    for (long long i = 0; i < n; i++)
     {
-      if (s[i] == '-')
-      {
-        v.push_back(-1);
-      }
+      if (s[i] == '+')
+        pos++;
       else
-      {
-        v.push_back(1);
-      }
+        pos--;
     }
-
-    sort(v.begin(), v.begin() + v.size());
-
-    // for (int i = 0; i < v.size(); i++)
-    // {
-    //   cout << v[i] << " ";
-    // }
-
-    int minus = 0;
-    int plus = 0;
-    for (int i = 0; i < v.size(); i++)
-    {
-      if (v[i] == -1)
-      {
-        minus++;
-      }
-      if (v[i] == 1)
-      {
-        plus++;
-      }
-    }
-    int diff = 0;
-    if (minus > plus)
-    {
-      diff = minus - plus
-    }
-    else
-    {
-      diff = plus - minus;
-    }
-
-    cout << diff << endl;
+    cout << abs(pos) << endl;
 
     cases--;
   }
