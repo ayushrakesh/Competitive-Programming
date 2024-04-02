c9dee3d6f3428a7bff3ef6c9ab43d2e8b0d4091a
Ayush Rakesh <ayushrakesh35711artlover@gmail.com>
2024-04-02 08:49:41 +0530

    JAgged Swaps

--- a.cpp
+++ a.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+bool isSorted(vector<int> &v)
+{
+  for (int i = 0; i < v.size() - 1; i++)
+  {
+    if (v[i] > v[i + 1])
+      return false;
+  }
+  return true;
+}
 int main()
 {
   // your code goes here
@@ -13,9 +21,22 @@ int main()
     int n;
     cin >> n;
 
-    if (n % 3 == 0)
-      cout << "Second" << endl;
+    vector<int> v;
+    int a = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+      cin >> a;
+      v.push_back(a);
+    }
+    int mini = *min_element(v.begin(), v.end());
+
+    if (isSorted(v))
+      cout << "YES" << endl;
+
+    else if (v[0] == mini)
+      cout << "YES" << endl;
     else
-      cout << "First" << endl;
+      cout << "NO" << endl;
   }
 }
