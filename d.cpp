7de0c1693698e42aae0146d4297050e257357d2a
Ayush Rakesh <ayushrakesh35711artlover@gmail.com>
2024-04-03 14:33:43 +0530

    Ambitious Kid

--- d.cpp
+++ d.cpp
@@ -2,50 +2,26 @@
 using namespace std;
 int main()
 {
-  int q = 0;
-  cin >> q;
 
-  for (int i = 0; i < q; i++)
+  /* code */
+  int n;
+  cin >> n;
+
+  int a = 0;
+  vector<int> v;
+
+  for (int i = 0; i < n; i++)
   {
-    int n;
-    cin >> n;
+    cin >> a;
 
-    vector<int> v;
-    int a = 0;
-
-    for (int j = 0; j < n; j++)
+    if (a < 0)
     {
-      cin >> a;
-      v.push_back(a);
+      a = -a;
     }
-
-    int resu[n];
-
-    // int sum = 0;
-
-    for (int k = 0; k < n; k++)
-    {
-      int res = 0;
-      int prefSum = 0;
-
-      unordered_map<int, int> mmp;
-
-      mmp[0] = 1;
-
-      for (int i = 0; i < v.size(); i++)
-      {
-        prefSum += v[i];
-        res += mmp[prefSum - v[k]];
-        mmp[prefSum]++;
-      }
-
-      resu[v[k] - 1] = res;
-    }
-
-    for (int l = 0; l < n; l++)
-    {
-      cout << resu[l] << " ";
-    }
-    cout << endl;
+    v.push_back(a);
   }
+
+  sort(v.begin(), v.end());
+
+  cout << v[0] << endl;
 }
